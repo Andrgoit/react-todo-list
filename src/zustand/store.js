b65c9275d180c83b304d6231507b2414a4e4ab4c
Andrgoit <stakgrom@gmail.com>
2026-04-08 16:32:48 +0300

    add zustand persist

--- src/zustand/store.js
+++ src/zustand/store.js
@@ -1,31 +1,37 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 import { toast } from "react-toastify";
 
 const initialState = { todos: [] };
 
-export const useTodosStore = create((set) => ({
-  ...initialState,
-  addTodo: (todo) =>
-    set((state) => {
-      toast.success("The task added!");
-      return { todos: [...state.todos, todo] };
+export const useTodosStore = create(
+  persist(
+    (set) => ({
+      ...initialState,
+      addTodo: (todo) =>
+        set((state) => {
+          toast.success("The task added!");
+          return { todos: [...state.todos, todo] };
+        }),
+      updateTodo: (id, newTodo) =>
+        set(({ todos }) => {
+          toast.info("The task updated!");
+          return {
+            todos: todos.map((todo) => (todo.id === id ? newTodo : todo)),
+          };
+        }),
+      deleteTodo: (id) =>
+        set(({ todos }) => {
+          toast.info("The task deleted!");
+          return { todos: todos.filter((todo) => todo.id !== id) };
+        }),
+      completeTodo: (id) =>
+        set(({ todos }) => ({
+          todos: todos.map((todo) =>
+            todo.id === id ? { ...todo, completed: !todo.completed } : todo,
+          ),
+        })),
     }),
-  updateTodo: (id, newTodo) =>
-    set(({ todos }) => {
-      toast.info("The task updated!");
-      return {
-        todos: todos.map((todo) => (todo.id === id ? newTodo : todo)),
-      };
-    }),
-  deleteTodo: (id) =>
-    set(({ todos }) => {
-      toast.info("The task deleted!");
-      return { todos: todos.filter((todo) => todo.id !== id) };
-    }),
-  completeTodo: (id) =>
-    set(({ todos }) => ({
-      todos: todos.map((todo) =>
-        todo.id === id ? { ...todo, completed: !todo.completed } : todo,
-      ),
-    })),
-}));
+    { name: "todos" },
+  ),
+);
